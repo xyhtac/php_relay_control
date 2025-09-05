47e71d995428de3327d8563641716f4fc4fb73f6
xyhtac <73641145+xyhtac@users.noreply.github.com>
2025-09-05 19:08:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -169,7 +169,7 @@ $(function(){
 			chan_status = relay_channel[ chan_id - 1 ];
 			
 			toggle_invert = $( this ).attr('io-relay-invert');
-			if (toggle_invert == 1) { 
+			if (toggle_invert === '1') { 
 				logic_base = 1;
 			} else {
 				logic_base = 0;
